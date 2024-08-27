a2d76f1f298e29f64f2df070789320d87592f91f
pNatTueku <nat.tueku@gmail.com>
2024-08-27 21:50:25 +0700

    commit

---
+++ hello2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello GO's World")
+}
+
+func Main() { // Go is case sensitive
+	fmt.Println(("Bye"))
+}
---
+++ hello3.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello GO's World")
+	fmt.Println("Bye")
+}
