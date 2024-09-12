ee2fef0ddb04c1755143a44f5fe1e147a12e55e1
pNatTueku <nat.tueku@gmail.com>
2024-09-13 06:16:49 +0700

    commit

---
+++ unit_test_say.go
@@ -0,0 +1,9 @@
+// Require one .go and another one .go for unit test
+
+package main
+
+import "fmt"
+
+func bye() {
+	fmt.Println("Bye")
+}
---
+++ unit_test_say_test.go
@@ -0,0 +1,12 @@
+// Require one .go and another one .go for unit test
+
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBye(t *testing.T) {
+	fmt.Println("Testing Ok...")
+}
